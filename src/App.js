5a8a8bf91d8f367c082d36aa749604a2a82096ba
David Fejes <35314157+fejes99@users.noreply.github.com>
2023-02-13 11:00:56 +0100

    Created Nations cards and search bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Nations from './Nations/Nations';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className='App'>
-      <div>Hello World</div>
-    </div>
-  );
+class App extends Component {
+  state = {
+    nations: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        'https://raw.githubusercontent.com/iamspruce/search-filter-painate-reactjs/main/data/countries.json'
+      )
+      .then((res) => {
+        this.setState({
+          nations: res.data,
+          loading: false,
+        });
+      });
+  }
+
+  searchHandler = (event) => {
+    const searchQuery = event.target.value;
+
+    const filteredNations = Object.values(this.state.nations).filter((nation) =>
+      nation.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    this.setState({ filteredNations: filteredNations });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='header'>
+          <input
+            type='text'
+            placeholder='Search for...'
+            onChange={(event) => this.searchHandler(event)}
+          />
+          <div>Hello World</div>
+        </header>
+        <Nations
+          nations={this.state.filteredNations || this.state.nations}
+          loader={this.state.loader}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Nations/Nations.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { ScaleLoader } from 'react-spinners';
+import './Nations.css';
+
+class Nations extends Component {
+  render() {
+    return (
+      <div className='cards'>
+        {this.props.loading ? (
+          <ScaleLoader color='#36d7b7' />
+        ) : (
+          Object.values(this.props.nations).map((nation) => (
+            <div key={nation.name} className='card'>
+              <img src={nation.flag.large} alt='flag' className='card-img' />
+              <div className='card-header'>{nation.name}</div>
+              <div className='card-content'>
+                Population:{' '}
+                <span className='text-grey'>
+                  {nation.population.toLocaleString('fi-FI')}
+                </span>
+              </div>
+              <div className='card-content'>
+                Region: <span className='text-grey'>{nation.region}</span>
+              </div>
+              <div className='card-content'>
+                Capital: <span className='text-grey'>{nation.capital}</span>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+    );
+  }
+}
+
+export default Nations;
