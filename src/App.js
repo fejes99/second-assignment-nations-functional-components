00b73fe55cf791767bba6cab8c26311629d2d65e
David Fejes <35314157+fejes99@users.noreply.github.com>
2023-02-24 11:49:13 +0100

    Functional components app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import Nations from './Nations/Nations';
-import axios from 'axios';
 import { searchFilter } from './helper';
 
 const App = () => {
