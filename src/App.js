159f6c7bae754ca682ed21b3171fcbc0472c27f5
David Fejes <35314157+fejes99@users.noreply.github.com>
2023-02-24 11:48:57 +0100

    App converted to functional components

--- src/App.js
+++ src/App.js
@@ -1,80 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
 import Nations from './Nations/Nations';
 import axios from 'axios';
 import { searchFilter } from './helper';
 
-class App extends Component {
-  state = {
-    nations: [],
-    loading: true,
-    regions: [],
-    selectedRegion: '',
-    searchQuery: '',
-  };
+const App = () => {
+  const [nations, setNations] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [regions, setRegions] = useState([]);
+  const [selectedRegion, setSelectedRegion] = useState('');
+  const [query, setQuery] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     axios
       .get(
         'https://raw.githubusercontent.com/iamspruce/search-filter-painate-reactjs/main/data/countries.json'
       )
       .then((res) => {
-        this.setState({
-          nations: Object.values(res.data),
-          loading: false,
-          regions: [...new Set(Object.values(res.data).map((nation) => nation.region))],
-        });
+        setNations(Object.values(res.data));
+        setLoading(false);
+        setRegions([...new Set(Object.values(res.data).map((nation) => nation.region))]);
       });
-  }
+  }, []);
 
-  searchHandler = (event) => {
-    this.setState(
-      {
-        searchQuery: event.target.value,
-      },
-      () => searchFilter(this.state.selectedRegion, this.state.nations, this.state.searchQuery)
+  const searchHandler = (query) => {
+    setQuery(query);
+  };
+
+  const optionHandler = (region) => {
+    setSelectedRegion(region);
+  };
+
+  const filteredNations = () => {
+    const filter = searchFilter(selectedRegion, nations, query);
+    return query !== '' && filter.length === 0 ? (
+      <div className='info'>Country doesn't exist, try another one</div>
+    ) : (
+      <Nations nations={filter} loading={loading} />
     );
   };
 
-  optionHandler = (event) => {
-    this.setState(
-      {
-        selectedRegion: event.target.value,
-      },
-      () => searchFilter(this.state.selectedRegion, this.state.nations, this.state.searchQuery)
-    );
-  };
-
-  render() {
-    const { loading, regions, selectedRegion, searchQuery } = this.state;
-    const nations = searchFilter(selectedRegion, this.state.nations, searchQuery);
-
-    return (
-      <div className='App'>
-        <header>
-          <input
-            value={searchQuery}
-            type='text'
-            placeholder='Search for...'
-            onChange={(event) => this.searchHandler(event)}
-          />
-          <select value={selectedRegion} onChange={(event) => this.optionHandler(event)}>
-            <option value=''>All regions</option>
-            {regions.map((region) => (
-              <option key={region} value={region}>
-                {region}
-              </option>
-            ))}
-          </select>
-        </header>
-        {searchQuery !== '' && nations.length === 0 ? (
-          <div className='info'>Country doesn't exist, try another one</div>
-        ) : (
-          <Nations nations={nations} loading={loading} />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className='App'>
+      <header>
+        <input
+          value={query}
+          type='text'
+          placeholder='Search for...'
+          onChange={(event) => searchHandler(event.target.value)}
+        />
+        <select value={selectedRegion} onChange={(event) => optionHandler(event.target.value)}>
+          <option value=''>All regions</option>
+          {regions.map((region) => (
+            <option key={region} value={region}>
+              {region}
+            </option>
+          ))}
+        </select>
+      </header>
+      {filteredNations()}
+    </div>
+  );
+};
 
 export default App;
