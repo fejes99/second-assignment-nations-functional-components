b439ddd4e69758b04fc0616857d293ab638d2193
David Fejes <35314157+fejes99@users.noreply.github.com>
2023-02-24 11:34:29 +0100

    Transforming class based components to functional components

--- src/Nations/Nations.js
+++ src/Nations/Nations.js
@@ -1,33 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ScaleLoader } from 'react-spinners';
 import './Nations.css';
 
-class Nations extends Component {
-  render() {
-    const { loading, nations } = this.props;
-    const nationsContent = loading ? (
-      <ScaleLoader color='#36d7b7' />
-    ) : (
-      nations.map((nation) => (
-        <div key={nation.name} className='card'>
-          <img src={nation.flag.large} alt='flag' className='card-img' />
-          <div className='card-header'>{nation.name}</div>
-          <div className='card-content'>
-            Population:{' '}
-            <span className='text-grey'>{nation.population.toLocaleString('fi-FI')}</span>
-          </div>
-          <div className='card-content'>
-            Region: <span className='text-grey'>{nation.region}</span>
-          </div>
-          <div className='card-content'>
-            Capital: <span className='text-grey'>{nation.capital}</span>
-          </div>
+const Nations = ({ loading, nations }) => {
+  const nationsContent = loading ? (
+    <ScaleLoader color='#36d7b7' />
+  ) : (
+    nations.map((nation) => (
+      <div key={nation.name} className='card'>
+        <img src={nation.flag.large} alt='flag' className='card-img' />
+        <div className='card-header'>{nation.name}</div>
+        <div className='card-content'>
+          Population: <span className='text-grey'>{nation.population.toLocaleString('fi-FI')}</span>
         </div>
-      ))
-    );
+        <div className='card-content'>
+          Region: <span className='text-grey'>{nation.region}</span>
+        </div>
+        <div className='card-content'>
+          Capital: <span className='text-grey'>{nation.capital}</span>
+        </div>
+      </div>
+    ))
+  );
 
-    return <div className='cards'>{nationsContent}</div>;
-  }
-}
+  return <div className='cards'>{nationsContent}</div>;
+};
 
 export default Nations;
