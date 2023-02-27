6a678973998b5ed7d5a287b790497417c628b64d
David Fejes <35314157+fejes99@users.noreply.github.com>
2023-02-27 13:25:11 +0100

    Updated loading

--- src/Nations/Nations.js
+++ src/Nations/Nations.js
@@ -3,25 +3,23 @@ import { ScaleLoader } from 'react-spinners';
 import './Nations.css';
 
 const Nations = ({ loading, nations }) => {
-  const nationsContent = loading ? (
-    <ScaleLoader color='#36d7b7' />
-  ) : (
-    nations.map((nation) => (
-      <div key={nation.name} className='card'>
-        <img src={nation.flag.large} alt='flag' className='card-img' />
-        <div className='card-header'>{nation.name}</div>
-        <div className='card-content'>
-          Population: <span className='text-grey'>{nation.population.toLocaleString('fi-FI')}</span>
-        </div>
-        <div className='card-content'>
-          Region: <span className='text-grey'>{nation.region}</span>
-        </div>
-        <div className='card-content'>
-          Capital: <span className='text-grey'>{nation.capital}</span>
-        </div>
+  if (loading) return <ScaleLoader color='#36d7b7' />;
+
+  const nationsContent = nations.map((nation) => (
+    <div key={nation.name} className='card'>
+      <img src={nation.flag.large} alt='flag' className='card-img' />
+      <div className='card-header'>{nation.name}</div>
+      <div className='card-content'>
+        Population: <span className='text-grey'>{nation.population.toLocaleString('fi-FI')}</span>
       </div>
-    ))
-  );
+      <div className='card-content'>
+        Region: <span className='text-grey'>{nation.region}</span>
+      </div>
+      <div className='card-content'>
+        Capital: <span className='text-grey'>{nation.capital}</span>
+      </div>
+    </div>
+  ));
 
   return <div className='cards'>{nationsContent}</div>;
 };
